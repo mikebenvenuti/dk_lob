513b8ad2c135a3b0643f933bc2d3fa9cd77b540c
mikebenvenuti <benvenuti@sbcglobal.net>
2016-01-12 21:04:36 -0600

    added a basic  route /items

--- app/app.js
+++ app/app.js
@@ -2,5 +2,22 @@
     "use strict";
     
     angular
-       .module("msbItems", ["common.services","itemResourceMock"]);
+       .module("msbItems", ["common.services","itemResourceMock","ui.router"])
+       .config(
+                ["$stateProvider", function($stateProvider) {
+                    $stateProvider
+                      .state("itemList", {
+                          url: "/items",
+                          templateUrl: "app/items/itemListView.html",
+                          controller: "ItemsCtrl as vm"
+                      })
+                    
+                }]
+           
+          ) 
+       
+       
+       
+       
+       
 }());
